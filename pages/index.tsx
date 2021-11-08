ec9eb8370273fb168e1c08b00896677f579dc87b
Sopruk <alexandr.programer@gmail.com>
2021-11-08 18:00:26 +0200

    added navbar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
+import Navbar from "../components/Navbar";
 import React from "react";
 
 const Index = () => {
-    return(
-        <h1>Hello NextJS!</h1>
-    )
+    return(<>
+    <Navbar/>
+    <div className="center">
+        <h1>Добро пожаловать!</h1>
+        <h3>Здесь только лучшие треки!</h3>
+    </div>
+    <style jsx>
+        {
+            `.center{
+                margin-top: 150px;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: cnter;
+            }`
+        }    
+    </style>    
+    </>)
 }
 export default Index;
