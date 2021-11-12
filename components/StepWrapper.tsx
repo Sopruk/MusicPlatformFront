39aad83e713a16fb870bcdc9fa54d4e14cb390b5
Sopruk <alexandr.programer@gmail.com>
2021-11-12 16:25:01 +0200

    finished StepWrapper

--- components/StepWrapper.tsx
+++ components/StepWrapper.tsx
@@ -1,14 +1,28 @@
-import { Container, Stepper } from '@material-ui/core'
+import { Container, Stepper, Step, StepLabel, Grid, Card } from '@material-ui/core'
+import { stepButtonClasses } from '@mui/material'
 import React from 'react'
 
 interface StepWrapperProps{
     activeStep: number
 }
-const steps = ["Treck informations", ""]
+const steps = ["Treck informations", "Track Label", "FIle track"]
 const StepWrapper: React.FC<StepWrapperProps> = ({activeStep, children}) => {
     return (
         <Container>
-            <Stepper activeStep={activeStep}>{children}</Stepper>
+            <Stepper activeStep={activeStep}>
+                {
+                    steps.map((step, index)=>
+                        <Step key={index} completed={activeStep>index}>
+                            <StepLabel>{step}</StepLabel>
+                        </Step>
+                    )
+                }
+            </Stepper>
+            <Grid container justifyContent="center" style={{margin:"70px 0", height:270}}>
+                <Card style={{width:600}}>
+                    {children}
+                </Card>
+            </Grid>
         </Container>
     )
 }
--- pages/tracks/create.tsx
+++ pages/tracks/create.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import { Button, Grid } from '@material-ui/core'
+import React, { useState } from 'react'
+import StepWrapper from '../../components/StepWrapper'
 import MainLayout from '../../layouts/MainLayout'
 
 const Create = () => {
+    const [activeStep, setActiveStep] = useState(0);
+    const back = () =>{
+        setActiveStep(prev => prev - 1)
+    }
+    const next = () =>{
+        const maxStep = 2;
+        if(activeStep<=maxStep)
+        setActiveStep( prev => prev + 1)
+    }
     return (
         <MainLayout>
-            <h1>Loading tracks</h1>
+            <StepWrapper activeStep={activeStep}>
+            <h1>Loading tracks- {activeStep}</h1>
+            </StepWrapper>
+            <Grid container justifyContent="space-between">
+                <Button variant="outlined" disabled={activeStep==0} onClick={back}>Prev</Button>
+                <Button variant="outlined" onClick={next}>Next</Button>
+            </Grid>
         </MainLayout>
     )
 }
