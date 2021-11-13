5e3aab6fc9f58c9061055acff64d6e5291b21376
Sopruk <alexandr.programer@gmail.com>
2021-11-13 15:45:33 +0200

    player is done

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,7 +1,11 @@
-import { IconButton } from '@material-ui/core'
-import { Pause, PlayArrow } from '@material-ui/icons'
+import { Grid, IconButton } from '@material-ui/core'
+import { Pause, PlayArrow, VolumeUp } from '@material-ui/icons'
 import React from 'react'
 import style from '../styles/Player.module.scss'
+import TrackProgres from './TrackProgres';
+
+const track = {_id:'2', name:'track_2', artist:'artist_1', text:'some text', listens:5, audio:'', picture:'https://cdnimg.rg.ru/pril/article/201/86/95/55p_gus-obtr_1000.jpg', comments:[{_id:'1', text:'some comment', username:'user_1'}] };
+
 const Player = () => {
     const active = false;
     return (
@@ -9,6 +13,13 @@ const Player = () => {
             <IconButton>
                 {!active?<PlayArrow/>:<Pause/>}
             </IconButton>
+            <Grid container direction="column" style={{width:200, margin:"0 20px"}}>
+                <div>{track.name}</div>
+                <div style={{fontSize:12, color:"grey"}}>{track.artist}</div>
+            </Grid>
+            <TrackProgres left={0} right={100} onChange={()=>({})}/>
+            <VolumeUp style={{marginLeft:"auto"}}/>
+            <TrackProgres left={0} right={100} onChange={()=>({})}/>
         </div>
     )
 }
---
+++ components/TrackProgres.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+interface TrackProgresProps{
+    left: number;
+    right: number;
+    onChange: (e) => void;
+}
+const TrackProgres: React.FC<TrackProgresProps> = ({left, right, onChange}) => {
+    return (
+        <div style={{display:"flex"}}>
+            <input 
+                type="range"
+                min={left}
+                max={right}
+                value={left}
+                onChange={onChange}
+             />
+             <div>{left} / {right}</div>
+        </div>
+    )
+}
+
+export default TrackProgres
